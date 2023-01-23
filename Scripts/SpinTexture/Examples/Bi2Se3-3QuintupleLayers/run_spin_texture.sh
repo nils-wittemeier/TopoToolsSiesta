2837d363b46cf1c65e841f96fb741e5039c5fbb0
Nils Wittemeier <nils.wittemeier@gmail.com>
2023-01-23 15:56:41 +0100

    Add example inputs

--- Scripts/SpinTexture/Examples/Bi2Se3-3QuintupleLayers/run_spin_texture.sh
+++ Scripts/SpinTexture/Examples/Bi2Se3-3QuintupleLayers/run_spin_texture.sh
@@ -1,11 +1,11 @@
 # Run all calculations
 for x in 'bands' 'circle' 'grid' 'constant_energy'; do
     echo "Calculation with mode ${x}"
-    python ../../../SpinTexture.py "input.${x}.json"
+    python ../../SpinTexture.py "input.${x}.json"
 done
 
 # Plot all results
 for x in 'bands' 'spinbands' 'circle' 'grid' 'constant_energy'; do
     echo "Plotting: ${x}"
-    python ../../../PlotSpinTexture.py "plot.${x}.json"
+    python ../../PlotSpinTexture.py "plot.${x}.json"
 done
